a7cdc3e25fd63dffad529f5c21601a55dc56aa70
ISTIEAK AHMED <54891541+istieak-a@users.noreply.github.com>
2024-02-16 00:52:03 +0600

    Update ServiceCard component with new design and images

--- src/Components/Home/ServiceCard.jsx
+++ src/Components/Home/ServiceCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { IoArrowForwardCircle } from "react-icons/io5";
 
-const ServiceCard = ({ title, desc }) => {
+const ServiceCard = ({ title, desc, img}) => {
   return (
-    <div className="flex w-full cursor-pointer items-center rounded-md p-5 shadow-md duration-500 hover:bg-[#3869e7] hover:text-white hover:shadow-none">
-      <div className="w-[90%]">
-        <h1 className="pb-3 text-2xl font-semibold">{title}</h1>
-        <p className="w-[80%] text-sm">{desc}</p>
+    <div className="flex w-[400px] cursor-pointer flex-col items-center gap-10 rounded-md py-10 shadow-md duration-500 hover:bg-[#3869e7] hover:text-white hover:shadow-none">
+      <img className="w-14" src={img} alt="" />
+      <div className="px-10 text-center">
+        <h1 className="pb-3 text-3xl font-semibold">{title}</h1>
+        <p className="text-sm">{desc}</p>
       </div>
-      <IoArrowForwardCircle className="text-3xl" />
+      {/* <IoArrowForwardCircle className="text-3xl" /> */}
     </div>
   );
 };
--- src/Components/Home/ServiceSection.jsx
+++ src/Components/Home/ServiceSection.jsx
@@ -1,46 +1,67 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
 import video from "../../assets/videoIn.svg";
+import fb_reels from "../../assets/fb_reels.png";
+import insta_reels from "../../assets/isnta_reels.png";
+import tiktok from "../../assets/tiktok.png";
+import yt_shorts from "../../assets/yt_shorts.png";
+import youtube from "../../assets/youtube.png";
+import facebook from "../../assets/fb_watch.png";
 
 const ServiceSection = () => {
   const services = [
     {
       title: "Facebook Reels",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+      desc: "Elevate your social media presence with captivating Facebook Reels. Our team crafts visually stunning content that sparks engagement and leaves a lasting impression on your audience.",
+      img: fb_reels,
     },
     {
       title: "Instagram Reels",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+      desc: "Ignite your Instagram feed with dynamic Reels that stop the scroll. From trending dances to behind-the-scenes moments, our experts ensure your content shines and connects with audiences worldwide.",
+      img: insta_reels,
     },
     {
       title: "Tiktok",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+      desc: "Unleash your creativity on TikTok with attention-grabbing videos that resonate with its vibrant community. Let us help you make waves on the world's fastest-growing video platform.",
+      img: tiktok,
     },
     {
-      title: "Youtube Shorts",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.",
+      title: "YouTube Shorts",
+      desc: "Step into the spotlight with YouTube Shorts and captivate a global audience in seconds. Our tailored content strategy ensures your Shorts leave a lasting impact and keep viewers coming back for more.",
+      img: yt_shorts,
+    },
+    {
+      title: "Facebook Video",
+      desc: "Engage and captivate your Facebook audience with expertly translated videos. Elevate your presence on the platform and connect with viewers around the globe with our professional content services.",
+      img: facebook,
+    },
+    {
+      title: "YouTube",
+      desc: "Unlock the full potential of YouTube with professionally translated videos that resonate with audiences worldwide. From in-depth tutorials to captivating storytelling, we help you reach new heights on the platform.",
+      img: youtube,
     },
   ];
+
   return (
-    <div className="flex w-full gap-10 border border-red-500 px-20 py-20">
-      <div className="flex w-2/5 flex-col border border-red-500">
-        {services.map((pd, index) => {
-          return <ServiceCard key={index} title={pd.title} desc={pd.desc} />;
-        })}
+    <section className="py-28 md:px-24 px-10">
+      <div className="flex w-full flex-col items-center gap-7 ">
+        <h1 className="md:text-7xl text-4xl text-center font-bold">OUR DISTRIBUTION</h1>
+        {/* <p className="w-[30%] border text-center">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam,
+          cupiditate.
+        </p> */}
       </div>
-      <div className="flex w-3/5 flex-col border border-red-600">
-        <div className="h-[40%]">
-          <h1 className="text-[10px] font-bold text-[#3d67ff]">OUR SERVICE</h1>
-          <h2 className="text-3xl font-bold text-[#3869e7]">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
-            nihil?
-          </h2>
-        </div>
-        <div className="flex h-[60%] items-center justify-center bg-blue-400">
-          <img className="w-[300px]" src={video} alt="Video" />
-        </div>
+      <div className="mt-32 flex flex-wrap justify-center gap-10">
+        {services.map((service, index) => (
+          <ServiceCard
+            key={index}
+            title={service.title}
+            desc={service.desc}
+            img={service.img}
+          />
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
