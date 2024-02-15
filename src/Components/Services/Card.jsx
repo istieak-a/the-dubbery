cef84099e522e6c442906fe64999cf5b040be45c
ISTIEAK AHMED <54891541+istieak-a@users.noreply.github.com>
2024-02-16 02:06:16 +0600

    Update Card component styling

--- src/Components/Services/Card.jsx
+++ src/Components/Services/Card.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Card = ({ img, title, desc }) => {
   return (
-    <div className="w-[300px] h-full flex flex-col gap-20">
-      <div className="h-[30%] border">
-        <img className="" src={img} alt="" />
+    <div className="w-[400px] flex flex-col gap-10 shadow-xl p-10 rounded-xl">
+      <div className="h-[40%] flex items-center justify-center">
+        <img className="w-[35%] object-cover" src={img} alt="" />
       </div>
-      <div className="h-[70%] border">
+      <div className="h-[60%] flex gap-3 flex-col justify-center items-center">
         <h1 className="text-xl font-semibold">{title}</h1>
-        <p className="text-xs">{desc}</p>
+        <p className="text-sm text-center">{desc}</p>
       </div>
     </div>
   );
